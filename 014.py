ed58c9e30ce6e568f5a595901be93adbaa41a215
ars1208 <ara.kaz1208@gmail.com>
2022-02-03 22:31:21 +0900

    add

--- 014.py
+++ 014.py
@@ -1,2 +1,14 @@
-n = int(input())
+import math
 
+n = int(input())
+ans = list()
+
+for i in range(2, int(math.sqrt(n))+1):
+    while n % i == 0:
+        n /= i
+        ans.append(i)
+
+if n >= 2:
+    ans.append(int(n))
+
+print(*ans)
