8bce2ce2795334798ba2d126425709bfda2b76f9
ars1208 <ara.kaz1208@gmail.com>
2022-02-11 22:57:14 +0900

    add

--- 020.py
+++ 020.py
@@ -1,3 +1,13 @@
 n = int(input())
-a = list(map(int,input().split()))
+A = list(map(int,input().split()))
 
+ans = 0
+for a in range(n):
+    for b in range(a+1,n):
+        for c in range(b+1,n):
+            for d in range(c+1,n):
+                for e in range(d+1,n):
+                    if A[a] + A[b] + A[c] + A[d] + A[e] == 1000:
+                        ans += 1
+
+print(ans)
