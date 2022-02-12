f384694be0f9b542a06e0c36accebd3061408f57
ars1208 <ara.kaz1208@gmail.com>
2022-02-12 14:46:09 +0900

    add

---
+++ 017.py
@@ -0,0 +1,16 @@
+n = int(input())
+a = list(map(int,input().split()))
+
+def euclid(x,y):
+    if y == 0:
+        return x
+    return euclid(y, x%y)
+
+x = a[0]
+y = a[1]
+ans = (x * y) // euclid(x,y)
+for i in range(2,n):
+    x = ans
+    y = a[i]
+    ans = (x * y) // euclid(x,y)
+print(ans)
