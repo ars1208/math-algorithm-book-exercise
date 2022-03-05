75b3e3869530ef40441ff35963c87fef847f9766
ars1208 <ara.kaz1208@gmail.com>
2022-03-05 20:15:27 +0900

    add

--- 030.py
+++ 030.py
@@ -4,5 +4,10 @@ for i in range(n):
     l = list(map(int,input().split()))
     loads.append(l)
 
+dp = [[-1]*n, [-1]*n]
 
+for i in range(n):
+    for j in range(w):
+        if j < loads[i][0]:
+            dp[i][j] = dp[i-1][j]
     
